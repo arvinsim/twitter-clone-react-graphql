6aa93c02bd8176eee4b12b121a1f551f2037b0c9
Arthur Vincent Simon <developarvin@gmail.com>
2017-05-01 08:58:41 +0800

    Update server so that the GraphQL endpoint can be accessed.

--- server/index.js
+++ server/index.js
@@ -3,7 +3,11 @@ import graphqlHTTP from 'express-graphql'
 import QuerySchema from './schemas/query'
 import compress from 'compression'
 
+import path from 'path'
 import webpack from 'webpack'
+import webpackMiddleware from 'webpack-dev-middleware'
+import webpackConfig from '../webpack.config.js'
+
 const debug = require('debug')('app:server')
 
 const app = express()
@@ -15,11 +19,11 @@ app.use(compress())
 // Apply Webpack HMR Middleware
 // ------------------------------------
 if (process.env.NODE_ENV === 'development') {
-  const compiler = webpack(require('../webpack.config.js'))
+  const compiler = webpack(webpackConfig)
 
   debug('Enabling webpack dev and HMR middleware')
-  app.use(require('webpack-dev-middleware')(compiler, {
-    publicPath: '',
+  app.use(webpackMiddleware(compiler, {
+    publicPath: webpackConfig.output.publicPath,
     contentBase: '',
     hot: true,
     lazy: false
@@ -27,23 +31,34 @@ if (process.env.NODE_ENV === 'development') {
   app.use(require('webpack-hot-middleware')(compiler, {
     path: '/__webpack_hmr'
   }))
-}
 
-app.get('/', (req, res) => {
-  res.send('Hello! This is the Twitter clone project using React, Relay and Redux')
-})
+  // GraphQL Endpoint
+  app.use('/graphql', graphqlHTTP({
+    schema: QuerySchema,
+    graphiql: true,
+    formatError: error => ({
+      message: error.message,
+      locations: error.locations,
+      stack: error.stack,
+      path: error.path
+    })
+  }))
 
-app.use('/graphql', graphqlHTTP({
-  schema: QuerySchema,
-  graphiql: true,
-  formatError: error => ({
-    message: error.message,
-    locations: error.locations,
-    stack: error.stack,
-    path: error.path
+  // This rewrites all routes requests to the root /index.html file
+  // (ignoring file requests). If you want to implement universal
+  // rendering, you'll want to remove this middleware.
+  app.use('*', function (req, res, next) {
+    const filename = path.join(compiler.outputPath, 'index.html')
+    compiler.outputFileSystem.readFile(filename, (err, result) => {
+      if (err) {
+        return next(err)
+      }
+      res.set('content-type', 'text/html')
+      res.send(result)
+      res.end()
+    })
   })
-}))
-
+}
 
 app.listen(5000)
 debug('Running GraphQL API server at localhost:5000/graphql')
